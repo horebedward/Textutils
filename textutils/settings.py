abac3b6cac31ef9a533a544b68c19e11c176c115
Horeb Edward <35797581+horebedward@users.noreply.github.com>
2020-07-04 22:42:01 +0500

    Update settings.py

--- textutils/settings.py
+++ textutils/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ykxurr2d9xy3nff6^1%uv7n10mfm@x+a&eg!17j_%&+&-v9emz'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
